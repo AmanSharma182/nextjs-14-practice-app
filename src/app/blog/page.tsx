b8544cf22d7583b8aba3c86f8057c5ba77266db8
AmanSharmaDuredev <aman.sharma@dure.dev>
2024-12-16 23:50:08 +0530

    Tutorial 18 - Completed

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "First blog"
+  }
+}
+
 export default function Blog() {
   return <h1>My Blog second</h1>
 }
