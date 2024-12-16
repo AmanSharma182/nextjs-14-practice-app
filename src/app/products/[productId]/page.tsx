03ab53140dd67c96a7643efdc2dd2e7fc564c03c
AmanSharmaDuredev <aman.sharma@dure.dev>
2024-12-16 23:39:19 +0530

    Tutorial 17 - Completed

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,24 @@
-export default function ProductDetails({params} : {
+import { Metadata } from "next";
+
+type Props = {
   params : {
-    productId: string
+    productId: string,
   }
-}) {
+};
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const title = await new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`Iphone ${params.productId}`)
+    }, 100);
+  });
+
+  return {
+    title: `Product ${title}`,
+  }
+}
+
+export default function ProductDetails({ params }: Props) {
   return (
     <>
       <h1>Product details : {params.productId}</h1>
