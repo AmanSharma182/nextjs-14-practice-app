d1157adf070de542aeb689a1410d0c957bd2dbcb
AmanSharmaDuredev <aman.sharma@dure.dev>
2024-12-17 12:38:35 +0530

    Tutorial 30 - Completed

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -2,19 +2,24 @@ export default function ComplexDashboardLayout({
   children,
   users,
   notifications,
-  revenue
+  revenue,
+  login
 }: {
   children: React.ReactNode,
   users: React.ReactNode,
   notifications: React.ReactNode,
   revenue: React.ReactNode,
+  login: React.ReactNode,
 }) {
-  return (
+  const isLogin = true;
+  return isLogin ? (
     <>
       <div>{children}</div>
       {users}
       {notifications}
       {revenue}
     </>
+  ) : (
+    login
   );
 }
